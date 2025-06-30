24df53f4699923de2a453895061c8f44b6f452ad
nitin-rao-oci <nitin.rao@oracle.com>
2025-06-30 14:27:22 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,17 @@
-def hello_hello():
-	return("Hello Hello!")
+def hello_hello( ):
+	"Return a greeting" # Docstring should use triple quotes
+	return ( "Hello Hello!" ) # Extra parentheses and spacing
 
-if __name__ == "__main__":
-	print(hello_hello())
+def add(x,y):return x+y # no spaces, one-liner
+
+def RepeatMessage(msg, times =2 ):
+    for i in range(times): print(msg) # one-line loop, bad spacing
+
+def    finalFunction():
+    print( "Done!" )
+
+if __name__=="__main__":
+    print(hello_hello( ))
+    result = add(2 ,3 )
+    RepeatMessage("Test",3)
+    finalFunction()
