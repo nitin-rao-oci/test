02bd18ed6ca079979213d68259f21ba76f67ae1c
nitin-rao-oci <nitin.rao@oracle.com>
2025-07-01 13:47:40 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,25 @@
-def HelloWorld (  ):
-    return "Hello, World!"  #Inline comment with no space
+def HelloWorld(   ):
+ return "Hello, World!"
 
-def goodbye_world(): print("Goodbye!") # Two statements on one line
+def goodbye_world(): 
+ print("Goodbye!") print("Again!")
 
-def   BAD_FUNCTION_name():
-  return( "This line has weird spacing" )
+def   BAD_FUNCTION_name(  ):
+  value = 1
+  if value = 1:
+    return( "This line has weird spacing" )  
+    print("Unreachable code")
 
-if __name__== "__main__":
-    HelloWorld()
-    goodbye_world(  )
+class Test:
+ def __init__(self): self.msg = "Bad class definition"
+
+ if True:
+  def nested():
+    print ("Nested inside class and if")
+
+if __name__=="__main__":
+    HelloWorld( )
+    goodbye_world()
     BAD_FUNCTION_name( )
+    x = 1
+    if x==1: print ("inline if")
