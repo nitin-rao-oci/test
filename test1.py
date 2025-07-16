989f138e5916328f620caadb910d0a35293c0415
Nitin Rao <nitin.rao@oracle.com>
2025-07-16 12:02:14 -0700

    fix pt 2

--- test1.py
+++ test1.py
@@ -13,27 +13,33 @@ def bad_function_123():
     for i in range(3):
         print(i) 
 
-class   badlyFormatted:
- def __init__(self):self.val= 0 
+class BadlyFormatted:
+    def __init__(self):
+        self.val = 0
 
- def DoThing(self ) :
-      if(self.val==0):print ("Doing the thing!")
+    def do_thing(self):
+            if self.val == 0:
+                print("Doing the thing!")
 
- def methodWithLogicError(self):
-    items = [1, 2, 3]
-    for i in range(len(items)):
-     print(items[i+1])
+    def method_with_logic_error(self):
+        items = [1, 2, 3]
+        for i in range(len(items) - 1):
+            print(items[i + 1])
 
-x=42;y= 13 ;z= x+y
+x = 42
+y = 13
+z = x + y
 
-def misplaced(): pass; return None
+def misplaced():
+    pass
 
-if __name__=="__main__":
- HelloWorld()
- GOODBYE_WORLD()
- anotherFunction(3 )
- __BADfunction123()
- obj = badlyFormatted()
- obj.DoThing()
- obj.methodWithLogicError()
- if z> 50 :print( "Big number" )
+if __name__ == "__main__":
+    print(hello_world())
+    goodbye_world()
+    print(another_function(3, 0))
+    bad_function_123()
+    obj = BadlyFormatted()
+    obj.do_thing()
+    obj.method_with_logic_error()
+    if z > 50:
+        print("Big number")
