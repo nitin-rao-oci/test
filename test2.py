fac3e37ed49136a81ca8a244e14d4e0d8c037b27
Nitin Rao <nitin.rao@oracle.com>
2025-07-16 11:57:51 -0700

    fix

--- test2.py
+++ test2.py
@@ -5,43 +5,46 @@ def  hello_hello( ):
 def add(x, y):
     return x + y
 
-def RepeatMessage(msg, times =2 ):
-    for i in range(times): print(msg)  # one-line loop, bad spacing
+def repeat_message(msg, times=2):
+    for _ in range(times):
+        print(msg)
 
-def    finalFunction():
+def final_function():
     message = "Done!"
-    for char in message: pass
+    for char in message:
+        pass
     return
-    print("Dead code")
 
 def infinite_loop():
     while True:
         pass
 
-def MIXEDcaseFunction(  arg1 ,arg2=5):return arg1+ arg2
+def mixed_case_function(arg1, arg2=5):
+    return arg1 + arg2
 
-class  MyClass:
- def __init__ ( self ):
-  self.value=10
+class MyClass:
+    def __init__(self):
+        self.value = 10
 
- def compute( self):
-  if(self.value>5): print( "Computing..." )
+    def compute(self):
+        if self.value > 5:
+            print("Computing...")
 
- def badIndent(self):
-  x=1
-   y=2
-  return x+y
+    def bad_indent(self):
+        x = 1
+        y = 2
+        return x + y
 
 x, y = 10, 20
 z = x + y
 
-if __name__=="__main__":
-    print(hello_hello( ))
-    result = add(2 ,3 )
-    RepeatMessage("Test",3)
-    finalFunction()
-    infinite_loop()
-    print(MIXEDcaseFunction(1))
+if __name__ == "__main__":
+    print(hello_hello())
+    result = add(2, 3)
+    repeat_message("Test", 3)
+    final_function()
+    # Note: infinite_loop() creates an endless loop; consider removing or handling termination.
+    print(mixed_case_function(1))
     obj = MyClass()
     obj.compute()
-    obj.badIndent()
+    obj.bad_indent()
